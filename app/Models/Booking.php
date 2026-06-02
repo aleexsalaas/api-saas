ca6689d795b4278e9c88c6be0160fcc6ae236db5
Alex Salas Galán <galsaan@MacBook-Air-de-Alex.local>
2026-06-02 09:34:05 +0200

    feat: añadido softDeletes en las booking

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Booking extends Model
 {
+
+    use HasFactory, SoftDeletes;
+
     protected $fillable = [
         'user_id',
         'room_id',
